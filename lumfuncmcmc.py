d2c67fd89749b67a14852cc97581286fb2c042e6
Astropianist <gxn75@psu.edu>
2022-04-21 12:04:00 -0400

    Removed trace

--- lumfuncmcmc.py
+++ lumfuncmcmc.py
@@ -174,7 +174,7 @@ class LumFuncMCMC:
         self.all_param_names = ['Lstar','phistar','sch_al','Flim','alpha']
         self.getRoot()
         self.setup_logging()
-        pdb.set_trace()
+        # pdb.set_trace()
 
     def setDLdVdz(self):
         ''' Create 1-D interpolated functions for luminosity distance (cm) and comoving volume differential (Mpc^3); also get function for minimum luminosity considered '''
