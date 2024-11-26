23268f196beeb09b3c2bd58426e9b84c12ebd2fe
Astropianist <gautam.nagaraj@epfl.ch>
2024-11-26 18:46:59 +0100

    Changing vgal name again back to original

--- run_lumfuncmcmc.py
+++ run_lumfuncmcmc.py
@@ -393,7 +393,7 @@ def main(argv=None):
                 print(f"For k={k} and kk={kk}:", ks_2samp(flux[k], flux[kk]))
     for i in range(len(flux)):
         alls_file_name = f'Likes_alls_field{args.field_name}_z{args.redshift}_mcf{args.min_comp_frac}_ll{args.lum_lim}_env{args.environment}_neb{len(flux)}_bin{i}_contam_{args.contam_lim}_cb{args.contambin}{args.extra_text}.pickle'
-        vgal_file_name = f'Likes_vgal_field{args.field_name}_z{args.redshift}_contam_{args.contam_lim}_cb{args.contambin}{args.extra_text}_new.pickle'
+        vgal_file_name = f'Likes_vgal_field{args.field_name}_z{args.redshift}_contam_{args.contam_lim}_cb{args.contambin}{args.extra_text}.pickle'
         print("Alls file name:", alls_file_name)
 
         # Initialize LumFuncMCMC class
